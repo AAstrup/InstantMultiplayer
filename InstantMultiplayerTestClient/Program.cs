dfc9f78d87464907721ba33fffbd7583a4b5204f
Alexander Westi Astrup <alexander.westi.astrup@gmail.com>
2021-07-18 01:22:24 +0200

    Exception logging in client

--- InstantMultiplayerTestClient/Program.cs
+++ InstantMultiplayerTestClient/Program.cs
@@ -12,27 +12,35 @@ namespace InstantMultiplayerTestClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Client World!");
-            var host = "localhost";
-            //var host = "127.0.0.1";
-            TcpClient tcpClient = new TcpClient(host, 61001);
-            var stream = tcpClient.GetStream();
-            var writer = new BinaryWriter(stream);
-            var reader = new BinaryReader(stream);
-            IFormatter formatter = new BinaryFormatter();
-            while (true) {
-                Console.WriteLine("Write line and press enter send that message to server");
-                var msg = Console.ReadLine();
+            try
+            {
 
-                var objectToSend = new TestMessage() { Message = msg };
-                byte[] bytes;
-                using (MemoryStream memory = new MemoryStream())
-                {
-                    formatter.Serialize(memory, objectToSend);
-                    bytes = memory.ToArray();
+                Console.WriteLine("Hello Client World!");
+                var host = "localhost";
+                //var host = "20.76.194.30"; -- Container instance
+                TcpClient tcpClient = new TcpClient(host, 61001);
+                var stream = tcpClient.GetStream();
+                var writer = new BinaryWriter(stream);
+                var reader = new BinaryReader(stream);
+                IFormatter formatter = new BinaryFormatter();
+                while (true) {
+                    Console.WriteLine("Write line and press enter send that message to server");
+                    var msg = Console.ReadLine();
+
+                    var objectToSend = new TestMessage() { Message = msg };
+                    byte[] bytes;
+                    using (MemoryStream memory = new MemoryStream())
+                    {
+                        formatter.Serialize(memory, objectToSend);
+                        bytes = memory.ToArray();
+                    }
+
+                    writer.Write(bytes);
                 }
-
-                writer.Write(bytes);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
         }
     }
